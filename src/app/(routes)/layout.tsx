c86372665720272ab551d1ef1cfa8d99df7bb0a0
immengzi <immengzi@outlook.com>
2024-10-30 01:30:57 +0800

    fix: prevent GET /api/auth/validate twice
    ref: https://zh-hans.react.dev/learn/you-might-not-need-an-effect#initializing-the-application

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -4,13 +4,18 @@ import React, {useEffect} from "react";
 import {useAuth} from "@/hooks/use-auth";
 import {Spinner} from "@/components/ui/Spinner";
 
+let didInit = false;
 export default function RoutesLayout({children}: {
     children: React.ReactNode
 }) {
     const {isLoading, validateSession} = useAuth();
 
     useEffect(() => {
-        validateSession()
+        if (!didInit) {
+            didInit = true;
+            // 只在每次应用加载时执行一次
+            validateSession();
+        }
     }, [])
 
     if (isLoading) {
